6fc773315f4a837e037673323963a3a659331f06
apriansyahrs <apriansyahrizs@gmail.com>
2025-10-03 16:52:34 +0700

    Add language files for error notifications, global search, layout, dashboard, tenant profile editing, record creation and editing, and multi-factor authentication in English and Indonesian

--- app/Filament/Pages/Auth/Login.php
+++ app/Filament/Pages/Auth/Login.php
@@ -32,7 +32,7 @@ class Login extends \Filament\Auth\Pages\Login
     protected function throwFailureValidationException(): never
     {
         throw ValidationException::withMessages([
-            'data.login' => __('filament-panels::pages/auth/login.messages.failed'),
+            'data.login' => __('filament-panels::auth/pages/login.messages.failed'),
         ]);
     }
 }

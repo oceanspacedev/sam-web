6d7c91be7ed7e9343a9b05c4f6eec8d9dad42045
apriansyahrs <apriansyahrizs@gmail.com>
2025-11-12 11:30:28 +0700

    fix: correct bitwise operations for UUID generation in FilenameGeneratorService

--- app/Services/FilenameGeneratorService.php
+++ app/Services/FilenameGeneratorService.php
@@ -145,8 +145,8 @@ class FilenameGeneratorService
 
         $hash = substr($hex, 0, 6);
 
-        $bytes[6] = chr((ord($bytes[6]) & 0x0f) | 0x40);
-        $bytes[8] = chr((ord($bytes[8]) & 0x3f) | 0x80);
+        $bytes[6] = chr((ord($bytes[6]) & 0x0F) | 0x40);
+        $bytes[8] = chr((ord($bytes[8]) & 0x3F) | 0x80);
 
         $uuid = vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($bytes), 4));
 
